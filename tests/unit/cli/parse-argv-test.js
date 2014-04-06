efe4234420cdc2fe1ef58bd0cc9b38c42d0d7d1e
Josef Brandl <mail@josefbrandl.de>
2014-04-07 00:47:08 +0200

    Command run restrictions

--- tests/unit/cli/parse-argv-test.js
+++ tests/unit/cli/parse-argv-test.js
@@ -15,6 +15,7 @@ describe('cli/parse-argv.js', function() {
         name: 'serve',
         key: 'serve',
         aliases: ['s'],
+        works: 'everywhere',
         options: [
           { name: 'port', key: 'port', type: Number, default: 4200, required: true }
         ]
@@ -23,15 +24,41 @@ describe('cli/parse-argv.js', function() {
         name: 'develop-ember-cli',
         key: 'developEmberCLI',
         aliases: [],
+        works: 'everywhere',
         options: [
           { name: 'package-name', key: 'packageName', type: String, required: true }
         ]
+      },
+      insideProject: {
+        name: 'inside-project',
+        key: 'insideProject',
+        aliases: [],
+        works: 'insideProject',
+        options: []
+      },
+      outsideProject: {
+        name: 'outside-project',
+        key: 'outsideProject',
+        aliases: [],
+        works: 'outsideProject',
+        options: []
+      },
+      everywhere: {
+        name: 'everywhere',
+        key: 'everywhere',
+        aliases: [],
+        works: 'everywhere',
+        options: []
       }
     },
     ui: new UI({
       inputStream: through(),
       outputStream: through(function(data) { output.push(data); })
-    })
+    }),
+    project: {
+      directory: '',
+      packageJSON: {}
+    }
   };
 
   var parse = parseArgv.bind(null, environment);
@@ -66,8 +93,29 @@ describe('cli/parse-argv.js', function() {
     });
   });
 
-  it('parseArgv() should print a message if a required option is missing', function() {
+  it('parseArgv() should print a message if a required option is missing.', function() {
     expect(parse(['ember', 'develop-ember-cli'])).to.be.null;
     expect(output.shift()).to.match(/requires the option.*package-name/);
+  });
+
+  it('parseArgv() should print a message if a task cannot need the presence/absence of a project.', function() {
+    output = [];
+
+    // Inside project
+    expect(parse(['ember', 'inside-project'])).to.exist;
+    expect(parse(['ember', 'outside-project'])).to.be.null;
+    expect(output.shift()).to.match(/You cannot use.*inside an ember-cli project/);
+    expect(parse(['ember', 'everywhere'])).to.exist;
+
+    // Outside project
+    var project = environment.project;
+    environment.project = null;
+
+    expect(parse(['ember', 'inside-project'])).to.null;
+    expect(output.shift()).to.match(/You have to be inside an ember-cli project/);
+    expect(parse(['ember', 'outside-project'])).to.be.exist;
+    expect(parse(['ember', 'everywhere'])).to.exist;
+
+    environment.project = project;
   });
 });
